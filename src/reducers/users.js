32274dfff71343637ef446aef5e394f9cfe95efd
John Young <john@codeandcreative.com>
2019-01-30 16:51:30 -0800

    bug: fix adding questions not updating the users list

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -3,12 +3,24 @@ import {
     USER_ANSWER_QUESTION
 } from '../actions/users';
 
+import { ADD_QUESTION } from '../actions/questions';
+
 export default function users(state = {}, action) {
     switch (action.type) {
         case RECEIVE_USERS:
             return ({
                 ...state,
                 ...action.users
+            });
+        case ADD_QUESTION:
+            return ({
+                ...state,
+                [action.question.author]: {
+                    ...state[action.question.author],
+                    questions: state[action.question.author].questions.indexOf(action.question.id) === -1
+                        ? state[action.question.author].questions.concat(action.question.id)
+                        : state[action.question.author].questions
+                }
             });
         case USER_ANSWER_QUESTION:
             if (Object.keys(state[action.authedUser].answers).indexOf(action.qid) === -1) {
