46c6cfb33373ec9ab98f241d86419ab1ece7b76c
John Young <john@codeandcreative.com>
2019-01-30 16:59:55 -0800

    refactor: QuestionPage is now a stateless component

--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -1,32 +1,30 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import Question from './Question';
 import NotFound from './NotFound';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { TiArrowBack } from 'react-icons/ti';
 
-class QuestionPage extends Component {
-    render() {
-        const { id } = this.props.match.params;
-        const { isValidQuestion } = this.props;
+function QuestionPage(props) {
+    const { id } = props.match.params;
+    const { isValidQuestion } = props;
 
-        return (
-            <Fragment>
-                {id && isValidQuestion
-                    ? (
-                        <div className='container' >
-                            <div className='row justify-content-center'>
-                                <div className='col-md-8'>
-                                    <button className='btn btn-link back-link' onClick={(e) => this.props.history.goBack()}><TiArrowBack className='back-arrow text-primary' /> Back</button>
-                                    <Question id={id} isSolo={true} />
-                                </div>
+    return (
+        <Fragment>
+            {id && isValidQuestion
+                ? (
+                    <div className='container' >
+                        <div className='row justify-content-center'>
+                            <div className='col-md-8'>
+                                <button className='btn btn-link back-link' onClick={(e) => props.history.goBack()}><TiArrowBack className='back-arrow text-primary' /> Back</button>
+                                <Question id={id} isSolo={true} />
                             </div>
-                        </div>)
-                    : <NotFound />
-                }
-            </Fragment>
-        )
-    }
+                        </div>
+                    </div>)
+                : <NotFound />
+            }
+        </Fragment>
+    )
 }
 
 function mapStateToProps({ questions }, props) {
